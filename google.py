ec8a927e6812d8a7599ad66c847215973e456ea4
Peter Downs <peter.l.downs@gmail.com>
2012-03-08 22:01:34 -0500

    Adds functions for creating and deleting portfolios

--- google.py
+++ google.py
@@ -21,29 +21,79 @@ def login(user):
 		user["headers"]["Authorization"] = ("GoogleLogin auth="+Auth)
 	return user
 
-def get_portfolios(__user):
+def make_portfolio(user, title, currencyCode):
+	port_str = "<entry xmlns='http://www.w3.org/2005/Atom' "\
+					"xmlns:gf='http://schemas.google.com/finance/2007'> "\
+		  			"<title>{}</title> "\
+		    		"<gf:portfolioData currencyCode='{}'/> "\
+				"</entry>"
+	port_str = port_str.format(title, currencyCode)
+	
+	headers = user['headers']
+	headers['content-type'] = 'application/atom+xml'
+
+	r = requests.post("https://finance.google.com/finance/feeds/default/portfolios?alt=json", headers=headers, data=port_str)
+	if r.status_code != 201:
+		print "THERE WAS A SERIOUS ERROR. THIS IS NOT A JOKE. YOUR PORTFOLIO WAS NOT CREATED"
+		print "... please take me seriously?"
+		return user
+	
+	resp_data = json.loads(r.content)
+	new_portfolio = make_port_from_entry(resp_data['entry'])
+	user['portfolios'][new_portfolio['title']] = new_portfolio
+	print "Portfolio created."
+	print_portfolio(new_portfolio)
+	
+	return user
+
+def delete_portfolio(user, title):
+	if title in user['portfolios']:
+		r = requests.delete(user['portfolios'][title]['link'], headers=user['headers'])
+		if r.status_code == 200:
+			print "Successfully deleted portfolio: {}".format(title)
+		else:
+			print "Wasn't able to delete due to some auth/header error, probably. Idk."
+	else: print "Unable to successfully delete portfolio: {}".format(title)
+
+def make_port_from_entry(entry):
+	"""Given a JSON entry from the google API, create a portfolio object"""
+
+	portData = {
+		'title' : entry['title']['$t'],
+		'updated' : entry['updated']['$t'],
+		'id' : entry['id']['$t'],
+		'etag' : entry['gd$etag'],
+		'link' : entry['link'][1]['href'],
+		'feedLink' : entry['gd$feedLink']['href'],
+		'portfolioData' : {}
+	}
+	portData['portfolioData'].update(entry['gf$portfolioData'])
+	for key in portData['portfolioData']:
+		if key != "currencyCode":
+			portData['portfolioData'][key] = float(portData['portfolioData'][key])
+	return portData
+
+def print_portfolio(portData):
+	print "{}:\n  Last Updated: {}\n  Link to feed: {}".format(portData['title'], portData['updated'], portData['link'])
+	for k, v in portData['portfolioData'].iteritems():
+		print "    {}  = {}".format(k, v)
+
+def get_portfolios(user):
 	reqstr = "https://finance.google.com/finance/feeds/default/portfolios?alt=json"
-	if not __user:
+	if not user or not user['Auth']:
 		print "Not authenticated!"
-	r = requests.get(reqstr, headers=__user['headers'])
+		return
+	r = requests.get(reqstr, headers=user['headers'])
 	print "Status code: %d" % r.status_code
 	resp_data = json.loads(r.content)
 	feed = resp_data['feed']
 	entries = feed['entry']
 	for entry in entries:
-		feedData = {
-			'title' : entry['title']['$t'],
-			'etag' : entry['gd$etag'],
-			'link' : entry['link'][1]['href'],
-			'feedLink' : entry['gd$feedLink']['href'],
-			'portfolioData' : {}
-		}
-		feedData['portfolioData'].update(entry['gf$portfolioData'])
-		for key in feedData['portfolioData']:
-			if key != "currencyCode":
-				feedData['portfolioData'][key] = float(feedData['portfolioData'][key])
-		print feedData
-
+		#print json.dumps(entry, sort_keys=True, indent=4)
+		portData = make_port_from_entry(entry)
+		user['portfolios'][portData['title']] = portData
+		print_portfolio(portData)
+	return user
 
 def session():
 	""" Starts an interactive sesion """
@@ -56,6 +106,7 @@ def session():
 		"service" : "finance",
 		"source" : "downsEllis-autoStock-1.0",
 		"Auth" : None,
+		"portfolios" : {},
 		"headers" : {
 			"GData-Version" : "2",
 			'content-type':'application/x-www-form-urlencoded',
@@ -70,7 +121,11 @@ def session():
 		print "... success."
 		print "Waiting for commands."
 	
-	get_portfolios(user)
+	user = get_portfolios(user)
+	import time
+	p_name = "Testing_"+str(time.time())
+	user = make_portfolio(user, p_name, "USD")
+	delete_portfolio(user, p_name)
 
 	while 0:
 		user_input = raw_input("> ")
@@ -87,7 +142,7 @@ def session():
 
 		for name, func in __COMS.iteritems():
 			if com.lower().strip() == name.lower().strip():
-				func(args, __user=user)
+				func(args, user=user)
 		if com == 'q':
 			print "Done."
 			break
